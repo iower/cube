815d5b1cea843d1693fe1f0a03f8a92b6f11b2f8
iower <iower@tutanota.com>
2022-12-17 11:25:26 +0500

    Better

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 const pages = [
   (<>
     <h2>Venus</h2>
-    <img style={{ float: 'left', padding: 20 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Venus_2_Approach_Image.jpg/330px-Venus_2_Approach_Image.jpg" />
+    <img style={{ float: 'left', padding: 20 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Venus_-_December_23_2016.png/330px-Venus_-_December_23_2016.png" />
     <p>
     Venus is the second planet from the Sun. It is sometimes called Earth's "sister" or "twin" planet as it is almost as large and has a similar composition. As an interior planet to Earth, Venus (like Mercury) appears in Earth's sky never far from the Sun, either as morning star or evening star. Aside from the Sun and Moon, Venus is the brightest natural object in Earth's sky, capable of casting visible shadows on Earth at dark conditions and being visible to the naked eye in broad daylight.
     </p>
@@ -23,14 +23,11 @@ const pages = [
     <p>
     The atmosphere of the Earth consists mostly of nitrogen and oxygen. Greenhouse gases in the atmosphere like carbon dioxide (CO2) trap a part of the energy from the Sun close to the surface. Water vapor is widely present in the atmosphere and forms clouds that cover most of the planet. More solar energy is received by tropical regions than polar regions and is redistributed by atmospheric and ocean circulation. A region's climate is governed by latitude, but also by elevation and proximity to moderating oceans. In most areas severe weather, such as tropical cyclones, thunderstorms, and heatwaves, occurs and greatly impacts life.
     </p>
-    <p>
-    Earth is an ellipsoid with a circumference of about 40,000 km. It is the densest planet in the Solar System. Of the four rocky planets, it is the largest and most massive. Earth is about eight light minutes away from the Sun and orbits it, taking a year (about 365.25 days) to complete one revolution. Earth rotates around its own axis in slightly less than a day (in about 23 hours and 56 minutes). Earth's axis of rotation is tilted with respect to the perpendicular to its orbital plane around the Sun, producing seasons. Earth is orbited by one permanent natural satellite, the Moon, which orbits Earth at 380,000 km (1.3 light seconds) and is roughly a quarter as wide as Earth. The Moon always faces the Earth with the same side through tidal locking and causes tides, stabilizes Earth's axis, and gradually slows its rotation.
-    </p>
   </>),
 
   (<>
     <h2>Mars</h2>
-    <img style={{ float: 'left', padding: 20 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Tharsis_and_Valles_Marineris_-_Mars_Orbiter_Mission_%2830055660701%29.png/330px-Tharsis_and_Valles_Marineris_-_Mars_Orbiter_Mission_%2830055660701%29.png" />
+    <img style={{ float: 'left', padding: 20, width: 300 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Mars_Valles_Marineris.jpeg/330px-Mars_Valles_Marineris.jpeg" />
     <p>
     Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In the English language, Mars is named for the Roman god of war. Mars is a terrestrial planet with a thin atmosphere (less than 1% that of Earth's), and has a crust primarily composed of elements similar to Earth's crust, as well as a core made of iron and nickel. Mars has surface features such as impact craters, valleys, dunes, and polar ice caps. It has two small and irregularly shaped moons: Phobos and Deimos.
     </p>
@@ -45,31 +42,29 @@ function App() {
   const [current, setCurrent] = useState(1)
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="screens">
-          {pages.map((page, i) => (
-            <div className={`
-              screen
-              ${i < current && 'prev'}
-              ${i === current && 'current'}
-              ${i > current && 'next'}
-            `}>
-              <div className="inner">
-                <div className="text">{page}</div>
-              </div>
+      <div className="screens">
+        {pages.map((page, i) => (
+          <div className={`
+            screen
+            ${i < current && 'prev'}
+            ${i === current && 'current'}
+            ${i > current && 'next'}
+          `}>
+            <div className="inner">
+              <div className="text">{page}</div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
 
-        <div className="buttons">
-          <button onClick={() => {
-            setCurrent(Math.max(current - 1, 0))
-          }}>&lt;--</button>
-          <button onClick={() => {
-            setCurrent(Math.min(current + 1, pages.length - 1))
-          }}>--&gt;</button>
-        </div>
-      </header>
+      <div className="buttons">
+        <button onClick={() => {
+          setCurrent(Math.max(current - 1, 0))
+        }}>⬅</button>
+        <button onClick={() => {
+          setCurrent(Math.min(current + 1, pages.length - 1))
+        }}>➡</button>
+      </div>
     </div>
   );
 }
